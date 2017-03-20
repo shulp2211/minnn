de0d3ae8757562c650e70a278923275ebd6a8020
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2017-03-20 15:14:30 +0300

    Initial commit.

---
+++ src/main/java/com/milaboratory/mist/Main.java
@@ -0,0 +1,7 @@
+package com.milaboratory.mist;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
---
+++ src/test/java/com/milaboratory/mist/MainTest.java
@@ -0,0 +1,12 @@
+package com.milaboratory.mist;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+    @Test
+    public void test1() throws Exception {
+        assertTrue(true);
+    }
+}
