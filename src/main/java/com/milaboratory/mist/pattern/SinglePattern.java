f7771206500d64e6c8dec8cc6843381cb6567187
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2017-03-23 12:23:06 +0300

    Extended SinglePattern#match() signature.

--- src/main/java/com/milaboratory/mist/pattern/SinglePattern.java
+++ src/main/java/com/milaboratory/mist/pattern/SinglePattern.java
@@ -1,9 +1,10 @@
 package com.milaboratory.mist.pattern;
 
+import com.milaboratory.core.Range;
 import com.milaboratory.core.sequence.MultiNSequenceWithQuality;
 import com.milaboratory.core.sequence.NSequenceWithQuality;
 
-public interface SinglePattern extends Pattern {
+interface SinglePattern extends Pattern {
     @Override
     default MatchingResult match(MultiNSequenceWithQuality input) {
         if (!(input instanceof NSequenceWithQuality))
@@ -12,8 +13,18 @@ public interface SinglePattern extends Pattern {
     }
 
     default MatchingResult match(NSequenceWithQuality input) {
-        return match(input, (byte)1);
+        return match(input, 0, input.size(), (byte) 1);
     }
 
-    MatchingResult match(NSequenceWithQuality input, byte targetId);
+    default MatchingResult match(NSequenceWithQuality input, Range range) {
+        if (range.isReverse())
+            throw new IllegalArgumentException("Doesn't support reversed ranges.");
+        return match(input, range.getFrom(), range.getTo());
+    }
+
+    default MatchingResult match(NSequenceWithQuality input, int from, int to) {
+        return match(input, from, to, (byte) 1);
+    }
+
+    MatchingResult match(NSequenceWithQuality input, int from, int to, byte targetId);
 }
