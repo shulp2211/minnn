37dc233d26408ee2acb43917762c40cc42195650
Alexander230 <fire_2005@mail.ru>
2017-03-29 14:31:31 +0300

    Removed unneeded import.

--- src/main/java/com/milaboratory/mist/pattern/Pattern.java
+++ src/main/java/com/milaboratory/mist/pattern/Pattern.java
@@ -1,7 +1,6 @@
 package com.milaboratory.mist.pattern;
 
 import com.milaboratory.core.sequence.MultiNSequenceWithQuality;
-import com.milaboratory.core.sequence.NSequenceWithQuality;
 
 public interface Pattern {
     MatchingResult match(MultiNSequenceWithQuality input);
