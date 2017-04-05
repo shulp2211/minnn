838cf5931d5534e189ef290bb7dc1632c7de24a5
Alexander230 <fire_2005@mail.ru>
2017-04-05 17:38:42 +0300

    Added random test, groups test and reverse autodetection test for MultiPattern.

--- src/test/java/com/milaboratory/mist/pattern/MultiPatternTest.java
+++ src/test/java/com/milaboratory/mist/pattern/MultiPatternTest.java
@@ -1,13 +1,20 @@
 package com.milaboratory.mist.pattern;
 
 import com.milaboratory.core.Range;
+import com.milaboratory.core.motif.Motif;
 import com.milaboratory.core.sequence.MultiNSequenceWithQuality;
 import com.milaboratory.core.sequence.NSequenceWithQuality;
 import com.milaboratory.core.sequence.NucleotideSequence;
+import com.milaboratory.core.sequence.SequenceQuality;
+import com.milaboratory.test.TestUtil;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.util.HashMap;
+import java.util.Random;
+
+import static com.milaboratory.mist.pattern.Match.COMMON_GROUP_NAME_PREFIX;
 import static org.junit.Assert.*;
 
 public class MultiPatternTest {
@@ -121,17 +128,92 @@ public class MultiPatternTest {
                 return null;
             }
         };
-        assertEquals(true, multiPattern.match(mseq, new Range[]{new Range(0, 11),
+        assertTrue(multiPattern.match(mseq, new Range[]{new Range(0, 11),
                 new Range(0, 10, true), new Range(0, 11, true)},
                 new boolean[]{false, true, true}).isFound());
-        assertEquals(true, multiPattern.match(mseq, false, true, true).isFound());
-        assertEquals(false, multiPattern.match(mseq, new Range[]{new Range(0, 11),
+        assertTrue(multiPattern.match(mseq, false, true, true).isFound());
+        assertFalse(multiPattern.match(mseq, new Range[]{new Range(0, 11),
                         new Range(1, 10, true), new Range(0, 11, true)},
                 new boolean[]{false, true, true}).isFound());
-        assertEquals(false, multiPattern.match(mseq, false, true, false).isFound());
+        assertFalse(multiPattern.match(mseq, false, true, false).isFound());
         assertEquals("GCATAT", multiPattern.match(mseq, false, true, true)
                 .getMatches().take().getWholePatternMatch(2).getValue().getSequence().toString());
         assertNull(multiPattern.match(mseq).getBestMatch());
         assertNotNull(multiPattern.match(mseq, false, true, true).getBestMatch());
+        assertTrue(multiPattern.match(mseq, new Range[]{new Range(0, 11),
+                new Range(0, 10, true), new Range(0, 11, true)}).isFound());
+    }
+
+    @Test
+    public void randomTest() throws Exception {
+        int its = TestUtil.its(500, 1000);
+        for (int i = 0; i < its; ++i) {
+            int sequencesNum = new Random().nextInt(9) + 1;
+            NSequenceWithQuality[] sequences = new NSequenceWithQuality[sequencesNum];
+            PerfectMatchPattern[] patterns = new PerfectMatchPattern[sequencesNum];
+            boolean isMatching = true;
+            for (int s = 0; s < sequencesNum; s++) {
+                NucleotideSequence seq = TestUtil.randomSequence(NucleotideSequence.ALPHABET, 1, 1000);
+                NucleotideSequence motifSeq = TestUtil.randomSequence(NucleotideSequence.ALPHABET, 1, 5);
+                Motif<NucleotideSequence> motif = new Motif<>(motifSeq);
+                NSequenceWithQuality currentTarget = new NSequenceWithQuality(seq, SequenceQuality
+                        .getUniformQuality(SequenceQuality.GOOD_QUALITY_VALUE, seq.getSequence().size()));
+                sequences[s] = currentTarget;
+                patterns[s] = new PerfectMatchPattern(motif);
+                isMatching = isMatching && seq.toString().contains(motifSeq.toString());
+            }
+            MultiNSequenceWithQuality mseq = new MultiNSequenceWithQuality() {
+                @Override
+                public int numberOfSequences() {
+                    return sequencesNum;
+                }
+
+                @Override
+                public NSequenceWithQuality get(int id) {
+                    return sequences[id];
+                }
+            };
+            MultiPattern multiPattern = new MultiPattern(patterns);
+            assertEquals(isMatching, multiPattern.match(mseq).isFound());
+        }
+    }
+
+    @Test
+    public void groupsTest() throws Exception {
+        HashMap<String, Range> groups1 = new HashMap<String, Range>() {{
+            put("1", new Range(0, 1));
+            put("2", new Range(1, 3));
+            put("4", new Range(4, 5));
+        }};
+        HashMap<String, Range> groups2 = new HashMap<String, Range>() {{
+            put("3", new Range(1, 3));
+            put("5", new Range(5, 6));
+        }};
+
+        PerfectMatchPattern pattern1 = new PerfectMatchPattern(new NucleotideSequence("TAGCC").toMotif(), groups1);
+        PerfectMatchPattern pattern2 = new PerfectMatchPattern(new NucleotideSequence("CAGATGCA").toMotif(), groups2);
+        MultiPattern multiPattern = new MultiPattern(pattern1, pattern2);
+        MultiNSequenceWithQuality mseq = new MultiNSequenceWithQuality() {
+            @Override
+            public int numberOfSequences() {
+                return 2;
+            }
+
+            @Override
+            public NSequenceWithQuality get(int id) {
+                switch (id) {
+                    case 0:
+                        return new NSequenceWithQuality("ACAATTAGCCA");
+                    case 1:
+                        return new NSequenceWithQuality("GTGCATCTGCCA");
+                }
+                return null;
+            }
+        };
+        MatchingResult result = multiPattern.match(mseq, false, true);
+        assertEquals("AG", result.getMatches(false).take().groupMatches.get(COMMON_GROUP_NAME_PREFIX + "2").getValue().getSequence().toString());
+        assertEquals(new Range(8, 9), result.getMatches(true).take().groupMatches.get(COMMON_GROUP_NAME_PREFIX + "5").getRange());
+        assertEquals("AG", result.getBestMatch().groupMatches.get(COMMON_GROUP_NAME_PREFIX + "3").getValue().getSequence().toString());
+        assertNull(result.getMatches().take());
     }
 }
