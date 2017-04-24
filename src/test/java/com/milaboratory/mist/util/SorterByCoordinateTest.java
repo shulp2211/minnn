5dc1361c7b9d2c4a7904b90c976f65c5a769fb66
Alexander230 <fire_2005@mail.ru>
2017-04-24 17:56:20 +0300

    Added matchesWithNullValuesTest for score and coordinate sorters.

--- src/test/java/com/milaboratory/mist/util/SorterByCoordinateTest.java
+++ src/test/java/com/milaboratory/mist/util/SorterByCoordinateTest.java
@@ -13,6 +13,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static com.milaboratory.mist.pattern.Match.WHOLE_PATTERN_MATCH_GROUP_NAME_PREFIX;
+import static com.milaboratory.mist.pattern.Match.COMMON_GROUP_NAME_PREFIX;
 import static com.milaboratory.mist.util.CommonTestTemplates.countPortValues;
 import static com.milaboratory.mist.util.CommonTestTemplates.predefinedMatchesApproximateSorterTest;
 import static org.junit.Assert.*;
@@ -72,9 +73,52 @@ public class SorterByCoordinateTest {
 
     @Test
     public void matchesWithNullValuesTest() throws Exception {
+        NSequenceWithQuality seq = new NSequenceWithQuality("TTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT");
+        Map<String, CaptureGroupMatch> testGroupsSingle = new HashMap<String, CaptureGroupMatch>() {{
+            put(WHOLE_PATTERN_MATCH_GROUP_NAME_PREFIX + "0", new CaptureGroupMatch(seq, (byte)1, new Range(0, 40)));
+            put(COMMON_GROUP_NAME_PREFIX + "0", new CaptureGroupMatch(seq, (byte)1, new Range(0, 40)));
+        }};
+        Map<String, CaptureGroupMatch> testGroupsMulti = new HashMap<String, CaptureGroupMatch>() {{
+            put(WHOLE_PATTERN_MATCH_GROUP_NAME_PREFIX + "0", new CaptureGroupMatch(seq, (byte)1, new Range(0, 40)));
+            put(COMMON_GROUP_NAME_PREFIX + "0", new CaptureGroupMatch(seq, (byte)1, new Range(0, 40)));
+            put(WHOLE_PATTERN_MATCH_GROUP_NAME_PREFIX + "1", new CaptureGroupMatch(seq, (byte)1, new Range(0, 40)));
+            put(COMMON_GROUP_NAME_PREFIX + "1", new CaptureGroupMatch(seq, (byte)1, new Range(0, 40)));
+        }};
 
+        Match testMatchSingle = new Match(1, 0, testGroupsSingle);
+        Match testMatchMulti = new Match(2, 0, testGroupsMulti);
+
+        TestMatchesOutputPort testPortSingle = new TestMatchesOutputPort(testMatchSingle);
+        TestMatchesOutputPort testPortMulti = new TestMatchesOutputPort(testMatchMulti);
+        TestMatchesOutputPort testPortEmpty = new TestMatchesOutputPort();
+
+        TestMatchesOutputPort[] testPortsSingleWithNull1 = new TestMatchesOutputPort[] {
+                testPortSingle.getCopy(), testPortEmpty};
+        TestMatchesOutputPort[] testPortsSingleWithNull2 = new TestMatchesOutputPort[] {
+                testPortEmpty, testPortSingle.getCopy()};
+        TestMatchesOutputPort[] testPortsMultiWithNull1Copy1 = new TestMatchesOutputPort[] {
+                testPortMulti.getCopy(), testPortEmpty};
+        TestMatchesOutputPort[] testPortsMultiWithNull1Copy2 = new TestMatchesOutputPort[] {
+                testPortMulti.getCopy(), testPortEmpty};
+        TestMatchesOutputPort[] testPortsMultiWithNull2Copy1 = new TestMatchesOutputPort[] {
+                testPortEmpty, testPortMulti.getCopy()};
+        TestMatchesOutputPort[] testPortsMultiWithNull2Copy2 = new TestMatchesOutputPort[] {
+                testPortEmpty, testPortMulti.getCopy()};
+
+        ApproximateSorter sorterSingle = new SorterByCoordinate(false, false, true,
+                false, MatchValidationType.INTERSECTION);
+        ApproximateSorter sorterMulti1 = new SorterByCoordinate(true, false, true,
+                false, MatchValidationType.ALWAYS);
+        ApproximateSorter sorterMulti2 = new SorterByCoordinate(true, true, true,
+                false, MatchValidationType.ALWAYS);
+
+        assertEquals(0, countPortValues(sorterSingle.getOutputPort(testPortsSingleWithNull1)));
+        assertEquals(0, countPortValues(sorterSingle.getOutputPort(testPortsSingleWithNull2)));
+        assertEquals(0, countPortValues(sorterMulti1.getOutputPort(testPortsMultiWithNull1Copy1)));
+        assertEquals(0, countPortValues(sorterMulti1.getOutputPort(testPortsMultiWithNull2Copy1)));
+        assertEquals(1, countPortValues(sorterMulti2.getOutputPort(testPortsMultiWithNull1Copy2)));
+        assertEquals(1, countPortValues(sorterMulti2.getOutputPort(testPortsMultiWithNull2Copy2)));
     }
-
 
     @Test
     public void matchesFromOperatorsTest() throws Exception {
