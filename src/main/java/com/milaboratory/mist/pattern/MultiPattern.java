c07cbc3058caeaaea44480d848ec3a8e01ea72b2
Alexander230 <fire_2005@mail.ru>
2017-05-03 16:14:25 +0300

    Bugfix for inversed ranges in MultiPattern.

--- src/main/java/com/milaboratory/mist/pattern/MultiPattern.java
+++ src/main/java/com/milaboratory/mist/pattern/MultiPattern.java
@@ -48,18 +48,21 @@ public class MultiPattern extends MultipleReadsOperator {
         public OutputPort<Match> getMatches(boolean byScore, boolean fairSorting) {
             ArrayList<OutputPort<Match>> operandPorts = new ArrayList<>();
             NSequenceWithQuality currentTarget;
+            Range currentRange;
             byte currentTargetId;
             ApproximateSorter sorter;
 
             for (int patternIndex = 0; patternIndex < singlePatterns.length; patternIndex++) {
                 if (reverseComplements[patternIndex]) {
                     currentTarget = target.get(patternIndex).getReverseComplement();
+                    currentRange = ranges[patternIndex].inverse();
                     currentTargetId = (byte) (-patternIndex - 1);
                 } else {
                     currentTarget = target.get(patternIndex);
+                    currentRange = ranges[patternIndex];
                     currentTargetId = (byte) (patternIndex + 1);
                 }
-                operandPorts.add(singlePatterns[patternIndex].match(currentTarget, ranges[patternIndex], currentTargetId)
+                operandPorts.add(singlePatterns[patternIndex].match(currentTarget, currentRange, currentTargetId)
                         .getMatches(byScore, fairSorting));
             }
 
