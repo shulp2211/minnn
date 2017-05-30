a5ec53f254709ab3e8e54b9c6a63d73eb6d71ac5
Alexander230 <fire_2005@mail.ru>
2017-05-30 17:59:27 +0300

    Added ParserFormat enum for normal and simplified parser formats.

--- src/main/java/com/milaboratory/mist/parser/Parser.java
+++ src/main/java/com/milaboratory/mist/parser/Parser.java
@@ -18,14 +18,19 @@ public final class Parser {
         this.errorScorePenalty = errorScorePenalty;
     }
 
+    public Pattern parseQuery(String query) throws ParserException {
+        return parseQuery(query, ParserFormat.NORMAL);
+    }
+
     /**
      * Main parser function that transforms query string to Pattern object. It will throw ParserException if something
      * is wrong in the query.
      *
      * @param query query string
+     * @param format parser format: NORMAL for end users or SIMPLIFIED as toString() output in inner classes
      * @return Pattern object for specified query string
      */
-    public Pattern parseQuery(String query) throws ParserException {
+    public Pattern parseQuery(String query, ParserFormat format) throws ParserException {
         if (query.equals("")) throw new IllegalArgumentException("Parser query is empty!");
         TokenizedString tokenizedString = new TokenizedString(query);
         return null;
---
+++ src/main/java/com/milaboratory/mist/parser/ParserFormat.java
@@ -0,0 +1,11 @@
+package com.milaboratory.mist.parser;
+
+/**
+ * String format for parser:
+ * "SIMPLIFIED": parsers and filters with their arguments in parentheses, as in their toString() methods
+ * "NORMAL": normal string format for end users
+ */
+public enum ParserFormat {
+    SIMPLIFIED,
+    NORMAL
+}
