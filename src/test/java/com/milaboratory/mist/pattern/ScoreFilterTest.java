aaf3c11b73e3fdd24bdbd04b0e148d9c74ae1312
Alexander230 <fire_2005@mail.ru>
2017-06-07 12:27:49 +0300

    Updated ScoreFilterTest to test MultipleReadsFilterPattern.

--- src/test/java/com/milaboratory/mist/pattern/ScoreFilterTest.java
+++ src/test/java/com/milaboratory/mist/pattern/ScoreFilterTest.java
@@ -1,6 +1,7 @@
 package com.milaboratory.mist.pattern;
 
 import cc.redberry.pipe.OutputPort;
+import com.milaboratory.core.sequence.MultiNSequenceWithQuality;
 import com.milaboratory.core.sequence.NSequenceWithQuality;
 import com.milaboratory.core.sequence.NucleotideSequence;
 import com.milaboratory.core.sequence.SequenceQuality;
@@ -26,10 +27,23 @@ public class ScoreFilterTest {
                     SequenceQuality.getUniformQuality(SequenceQuality.GOOD_QUALITY_VALUE, target.getSequence().size()));
             FuzzyMatchPattern pattern = new FuzzyMatchPattern(getTestPatternAligner(randomGenerator.nextInt(5)), motif);
             FilterPattern filterPattern = new FilterPattern(getTestPatternAligner(), new ScoreFilter(scoreThreshold), pattern);
+            MultiPattern multiPattern = new MultiPattern(getTestPatternAligner(), pattern, filterPattern);
+            MultipleReadsFilterPattern mFilterPattern = new MultipleReadsFilterPattern(getTestPatternAligner(),
+                    new ScoreFilter(scoreThreshold * 2), multiPattern);
+            AndOperator andOperator = new AndOperator(getTestPatternAligner(), multiPattern, mFilterPattern);
             boolean isMatching = pattern.match(targetQ).isFound()
                     && (pattern.match(targetQ).getBestMatch(true).getScore() >= scoreThreshold);
 
             assertEquals(isMatching, filterPattern.match(targetQ).isFound());
+            assertEquals(isMatching, andOperator.match(new MultiNSequenceWithQuality() {
+                @Override
+                public int numberOfSequences() {
+                    return 2;
+                }
+                @Override
+                public NSequenceWithQuality get(int id) {
+                    return targetQ;
+                }}).isFound());
             assertTrue(countMatches(pattern.match(targetQ)) >= countMatches(filterPattern.match(targetQ)));
             Match currentMatch;
             for (OutputPort<Match> filteredPort = filterPattern.match(targetQ).getMatches(
