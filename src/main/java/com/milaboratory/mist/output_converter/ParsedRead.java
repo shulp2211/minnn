951831833b30a923f55d10a81379c1f5506a2f64
Alexander230 <fire_2005@mail.ru>
2017-07-06 16:36:27 +0300

    ParsedRead changed to bean class.

--- src/main/java/com/milaboratory/mist/output_converter/ParsedRead.java
+++ src/main/java/com/milaboratory/mist/output_converter/ParsedRead.java
@@ -4,7 +4,20 @@ import com.milaboratory.core.io.sequence.MultiRead;
 
 import java.util.List;
 
-public interface ParsedRead {
-    MultiRead read();
-    List<MatchedGroup> groups();
+public final class ParsedRead {
+    private final MultiRead read;
+    private final List<MatchedGroup> groups;
+
+    public ParsedRead(MultiRead read, List<MatchedGroup> groups) {
+        this.read = read;
+        this.groups = groups;
+    }
+
+    public MultiRead read() {
+        return read;
+    }
+
+    public List<MatchedGroup> groups() {
+        return groups;
+    }
 }
