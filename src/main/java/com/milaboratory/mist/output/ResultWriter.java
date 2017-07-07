b521278e79fb180506cc0cae09e5a1d3ac55fe56
Alexander230 <fire_2005@mail.ru>
2017-07-07 16:57:44 +0300

    Implemented simple ResultWriter.

--- src/main/java/com/milaboratory/mist/output/ResultWriter.java
+++ src/main/java/com/milaboratory/mist/output/ResultWriter.java
@@ -1,9 +1,14 @@
 package com.milaboratory.mist.output;
 
+import com.milaboratory.core.io.sequence.MultiRead;
+import com.milaboratory.core.io.sequence.fastq.SingleFastqWriter;
 import com.milaboratory.mist.output_converter.ParsedRead;
 import com.milaboratory.mist.output_converter.ParsedReadsPort;
 
+import java.io.IOException;
 import java.util.List;
+
+import static com.milaboratory.mist.util.SystemUtils.exitWithError;
 
 public class ResultWriter {
     /**
@@ -16,9 +21,24 @@ public class ResultWriter {
     public static void writeResultsFromPort(List<String> fileNames, ParsedReadsPort port) {
         ParsedRead bestParsedRead = port.take();
         if (bestParsedRead != null)
-            writeResult(fileNames, bestParsedRead);
+            try {
+                writeResult(fileNames, bestParsedRead);
+            } catch (IOException e) {
+                System.err.println("I/O exception!");
+                exitWithError(e.getMessage());
+            }
     }
 
-    private static void writeResult(List<String> fileNames, ParsedRead parsedRead) {
+    private static void writeResult(List<String> fileNames, ParsedRead parsedRead) throws IOException {
+        MultiRead multiRead = parsedRead.read();
+        int numberOfReads = multiRead.numberOfReads();
+        if (fileNames.size() != numberOfReads)
+            exitWithError("Mismatched number of reads (" + numberOfReads + ") and number of output files ("
+                    + fileNames.size() + ")!");
+        for (int i = 0; i < numberOfReads; i++) {
+            SingleFastqWriter writer = new SingleFastqWriter(fileNames.get(i));
+            writer.write(multiRead.getRead(i));
+            writer.close();
+        }
     }
 }
