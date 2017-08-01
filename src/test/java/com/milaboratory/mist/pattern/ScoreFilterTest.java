66b5bf51593392f0673985438b587fd5d3e7ac50
Alexander230 <fire_2005@mail.ru>
2017-08-01 17:49:32 +0300

    Fixes in ScoreFilterTest.

--- src/test/java/com/milaboratory/mist/pattern/ScoreFilterTest.java
+++ src/test/java/com/milaboratory/mist/pattern/ScoreFilterTest.java
@@ -4,7 +4,6 @@ import cc.redberry.pipe.OutputPort;
 import com.milaboratory.core.sequence.MultiNSequenceWithQuality;
 import com.milaboratory.core.sequence.NSequenceWithQuality;
 import com.milaboratory.core.sequence.NucleotideSequence;
-import com.milaboratory.core.sequence.SequenceQuality;
 import com.milaboratory.test.TestUtil;
 import org.junit.Test;
 
@@ -17,33 +16,34 @@ public class ScoreFilterTest {
     public void randomTest() throws Exception {
         for (int i = 0; i < 1000; i++) {
             int scoreThreshold = -rg.nextInt(100);
-            NucleotideSequence target = TestUtil.randomSequence(NucleotideSequence.ALPHABET, 1, 1000);
+            NSequenceWithQuality target = new NSequenceWithQuality(TestUtil.randomSequence(NucleotideSequence.ALPHABET,
+                    1, 300).toString());
+            MultiNSequenceWithQuality multiTarget = new MultiNSequenceWithQuality() {
+                @Override
+                public int numberOfSequences() {
+                    return 2;
+                }
+                @Override
+                public NSequenceWithQuality get(int id) {
+                    return target;
+                }};
             NucleotideSequence motif = TestUtil.randomSequence(NucleotideSequence.ALPHABET, 1, 20);
-            NSequenceWithQuality targetQ = new NSequenceWithQuality(target,
-                    SequenceQuality.getUniformQuality(SequenceQuality.GOOD_QUALITY_VALUE, target.getSequence().size()));
             FuzzyMatchPattern pattern = new FuzzyMatchPattern(getTestPatternAligner(rg.nextInt(5)), motif);
             FilterPattern filterPattern = new FilterPattern(getTestPatternAligner(), new ScoreFilter(scoreThreshold), pattern);
             MultiPattern multiPattern = new MultiPattern(getTestPatternAligner(), pattern, filterPattern);
             MultipleReadsFilterPattern mFilterPattern = new MultipleReadsFilterPattern(getTestPatternAligner(),
                     new ScoreFilter(scoreThreshold * 2), multiPattern);
             AndOperator andOperator = new AndOperator(getTestPatternAligner(), multiPattern, mFilterPattern);
-            boolean isMatching = pattern.match(targetQ).isFound()
-                    && (pattern.match(targetQ).getBestMatch(true).getScore() >= scoreThreshold);
+            Match patternBestMatch = pattern.match(target).getBestMatch(true);
+            boolean isMatching = (patternBestMatch != null) && (patternBestMatch.getScore() >= scoreThreshold);
 
-//            assertEquals(isMatching, filterPattern.match(targetQ).getBestMatch(true) != null);
-//            assertEquals(isMatching, andOperator.match(new MultiNSequenceWithQuality() {
-//                @Override
-//                public int numberOfSequences() {
-//                    return 2;
-//                }
-//                @Override
-//                public NSequenceWithQuality get(int id) {
-//                    return targetQ;
-//                }}).getBestMatch(true) != null);
-//            assertTrue(countMatches(pattern.match(targetQ), true)
-//                    >= countMatches(filterPattern.match(targetQ), true));
+            assertEquals(isMatching, filterPattern.match(target).getBestMatch(true) != null);
+            if (countMatches(multiPattern.match(multiTarget), true) < 300)
+                assertEquals(isMatching, andOperator.match(multiTarget).getBestMatch(true) != null);
+            assertTrue(countMatches(pattern.match(target), true)
+                    >= countMatches(filterPattern.match(target), true));
             Match currentMatch;
-            for (OutputPort<Match> filteredPort = filterPattern.match(targetQ).getMatches(
+            for (OutputPort<Match> filteredPort = filterPattern.match(target).getMatches(
                     rg.nextBoolean(), rg.nextBoolean()); (currentMatch = filteredPort.take()) != null;)
                 assertTrue(currentMatch.getScore() >= scoreThreshold);
         }
