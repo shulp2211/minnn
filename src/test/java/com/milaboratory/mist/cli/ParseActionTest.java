84c29bb6df034ecfe406c83509516549295830ea
Alexander230 <fire_2005@mail.ru>
2017-08-11 17:28:56 +0300

    Added Normal syntax queries to ParseActionTest.

--- src/test/java/com/milaboratory/mist/cli/ParseActionTest.java
+++ src/test/java/com/milaboratory/mist/cli/ParseActionTest.java
@@ -24,18 +24,30 @@ public class ParseActionTest {
     public void simpleTest() throws Exception {
         String testInputR1 = TEST_RESOURCES_PATH + "sample_r1.fastq";
         String testInputR2 = TEST_RESOURCES_PATH + "sample_r2.fastq";
-        String testOutputR1 = TEST_RESOURCES_PATH + "output_r1.fastq";
-        String testOutputR2 = TEST_RESOURCES_PATH + "output_r2.fastq";
-        String testOutputSingle = TEST_RESOURCES_PATH + "output_single.fastq";
+        String testOutput1R1 = TEST_RESOURCES_PATH + "output1_r1.fastq";
+        String testOutput1R2 = TEST_RESOURCES_PATH + "output1_r2.fastq";
+        String testOutput1Single = TEST_RESOURCES_PATH + "output1_single.fastq";
+        String testOutput2R1 = TEST_RESOURCES_PATH + "output2_r1.fastq";
+        String testOutput2R2 = TEST_RESOURCES_PATH + "output2_r2.fastq";
+        String testOutput2Single = TEST_RESOURCES_PATH + "output2_single.fastq";
 
         String[] args1 = {"parse", "--pattern",
                 inQuotes("MultiPattern([FuzzyMatchPattern(GAAGCA, 1, 0, -1, -1, [GroupEdgePosition(" +
                         "GroupEdge('UMI', true), 2), GroupEdgePosition(GroupEdge('UMI', false), 4)]), " +
                         "FuzzyMatchPattern(AA, 0, 0, -1, -1)])"),
-                "--input", testInputR1, testInputR2, "--output", testOutputR1, testOutputR2, "--devel-parser-syntax"};
+                "--input", testInputR1, testInputR2, "--output", testOutput1R1, testOutput1R2, "--devel-parser-syntax"};
         main(args1);
+
         String[] args2 = {"parse", "--devel-parser-syntax", "--match-score", "0", "--oriented", "--pattern", inQuotes(
-                "FuzzyMatchPattern(ATTAGACA, 0, 0, -1, -1)") , "--input", testInputR1, "--output", testOutputSingle};
+                "FuzzyMatchPattern(ATTAGACA, 0, 0, -1, -1)"), "--input", testInputR1, "--output", testOutput1Single};
         main(args2);
+
+        String[] args3 = {"parse", "--pattern", "<GA(UMI:AG)CA \\ AA",
+                "--input", testInputR1, testInputR2, "--output", testOutput2R1, testOutput2R2};
+        main(args3);
+
+        String[] args4 = {"parse", "--match-score", "0", "--oriented", "--pattern", "ATTAGACA",
+                "--input", testInputR1, "--output", testOutput2Single};
+        main(args4);
     }
 }
