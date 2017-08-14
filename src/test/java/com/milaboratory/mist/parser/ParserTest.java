3e662cfda6a18fc1d9750df5d23a35075a5e5e1e
Alexander230 <fire_2005@mail.ru>
2017-08-14 16:30:40 +0300

    New tests for Normal syntax parser.

--- src/test/java/com/milaboratory/mist/parser/ParserTest.java
+++ src/test/java/com/milaboratory/mist/parser/ParserTest.java
@@ -136,11 +136,12 @@ public class ParserTest {
         testBadSample("AAT{2}^>");
         testBadSample("(U MI:AAA)");
         testSample("AT(1:TA + GA)CA", "ATTAGACA", new Range(0, 8));
-        testSample("A(1:TT)A || G(1:AC)A", "ATTA", new Range(0, 4));
+        testSample("A (1:TT) A || G (1:AC) A", "ATTA", new Range(0, 4));
         testBadSample("AT(1:TA & GA)CA");
         testBadSample("AT(1:TA || GA)CA");
         testBadSample("A(1:TT)A + G(1:AC)A");
         testBadSample("A(1:TT)A & G(1:AC)A");
+        testBadSample("[4:N]0");
     }
 
     @Test
@@ -170,6 +171,8 @@ public class ParserTest {
         testMultiSample("[A(1:TT)A \\ G(2:AC)A] || [(1:A) \\ (2:T)]", "A GACA", false);
         testBadSample("[A(1:TT)A \\ G(2:AC)A] && [(1:A) \\ (2:T)]");
         testBadSample("A(1:TT)A \\ G(1:AC)A");
+        testBadSample("T~A&&");
+        testBadSample("T~[A&&A]");
     }
 
     @Test
