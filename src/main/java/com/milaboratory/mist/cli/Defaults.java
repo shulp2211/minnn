2ea8ab7b6d1bf7c243188254601e9de57fa355ee
Alexander230 <fire_2005@mail.ru>
2017-09-01 23:05:04 +0300

    DEFAULT_SINGLE_OVERLAP_PENALTY set to DEFAULT_GAP_SCORE.

--- src/main/java/com/milaboratory/mist/cli/Defaults.java
+++ src/main/java/com/milaboratory/mist/cli/Defaults.java
@@ -5,12 +5,12 @@ import com.milaboratory.core.sequence.NucleotideSequence;
 
 public final class Defaults {
     public final static long DEFAULT_PENALTY_THRESHOLD = -30;
-    public final static long DEFAULT_SINGLE_OVERLAP_PENALTY = -1;
     public final static int DEFAULT_BITAP_MAX_ERRORS = 2;
     public final static Alphabet<NucleotideSequence> DEFAULT_ALPHABET = NucleotideSequence.ALPHABET;
     public final static int DEFAULT_MATCH_SCORE = 0;
     public final static int DEFAULT_MISMATCH_SCORE = -9;
     public final static int DEFAULT_GAP_SCORE = -10;
+    public final static long DEFAULT_SINGLE_OVERLAP_PENALTY = DEFAULT_GAP_SCORE;
     public final static int DEFAULT_THREADS = 4;
     public final static String DEFAULT_INPUT_FORMAT = "fastq";
     public final static String DEFAULT_OUTPUT_FORMAT = "fastq";
