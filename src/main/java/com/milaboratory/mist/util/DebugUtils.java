a4841863a18ee2eafd1e6062dedaff8c2a326544
Alexander230 <fire_2005@mail.ru>
2017-09-28 16:52:34 +0300

    Improved tools for debugging and profiling.

--- src/main/java/com/milaboratory/mist/util/DebugUtils.java
+++ src/main/java/com/milaboratory/mist/util/DebugUtils.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Callable;
 public final class DebugUtils {
     public static HashMap<String, Long> timeCounter = new HashMap<>();
     public static HashMap<String, Long> callCounter = new HashMap<>();
+    public static HashMap<String, Long> eventCounter = new HashMap<>();
 
     public static void printExecutionTime(String label, Callable<Void> f) {
         long startTime = System.currentTimeMillis();
@@ -30,9 +31,29 @@ public final class DebugUtils {
         timeCounter.put(label, (totalElapsedTime == null) ? elapsedTime : totalElapsedTime + elapsedTime);
     }
 
+    public static Object countExecutionTimeR(String label, Callable<Object> f) {
+        long startTime = System.currentTimeMillis();
+        Object o;
+        try {
+            o = f.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        Long totalElapsedTime = timeCounter.get(label);
+        timeCounter.put(label, (totalElapsedTime == null) ? elapsedTime : totalElapsedTime + elapsedTime);
+
+        return o;
+    }
+
     public static void countCall(String label) {
         Long count = callCounter.get(label);
         callCounter.put(label, (count == null) ? 1 : count + 1);
+    }
+
+    public static void countEvent(String label) {
+        Long count = eventCounter.get(label);
+        eventCounter.put(label, (count == null) ? 1 : count + 1);
     }
 
     public static void resetTimeCounter() {
