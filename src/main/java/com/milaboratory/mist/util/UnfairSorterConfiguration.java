a7853b811657826f40ad6095d6dad44ddc0323db
Alexander230 <fire_2005@mail.ru>
2017-09-28 16:50:58 +0300

    Tweaking unfair sorter configuration.

--- src/main/java/com/milaboratory/mist/util/UnfairSorterConfiguration.java
+++ src/main/java/com/milaboratory/mist/util/UnfairSorterConfiguration.java
@@ -7,20 +7,20 @@ import java.util.HashMap;
 public final class UnfairSorterConfiguration {
     public static final HashMap<Class, Integer> unfairSorterPortLimits = new HashMap<>();
     static {
-        unfairSorterPortLimits.put(FuzzyMatchPattern.class, 500);
-        unfairSorterPortLimits.put(RepeatPattern.class, 500);
+        unfairSorterPortLimits.put(FuzzyMatchPattern.class, 100);
+        unfairSorterPortLimits.put(RepeatPattern.class, 100);
         unfairSorterPortLimits.put(AnyPattern.class, 1);
-        unfairSorterPortLimits.put(FilterPattern.class, 50);
+        unfairSorterPortLimits.put(FilterPattern.class, 25);
         unfairSorterPortLimits.put(AndPattern.class, 20);
         unfairSorterPortLimits.put(PlusPattern.class, 20);
         unfairSorterPortLimits.put(SequencePattern.class, 20);
-        unfairSorterPortLimits.put(OrPattern.class, 50);
+        unfairSorterPortLimits.put(OrPattern.class, 25);
         unfairSorterPortLimits.put(MultiPattern.class, 25);
         unfairSorterPortLimits.put(AndOperator.class, 25);
         unfairSorterPortLimits.put(OrOperator.class, 25);
         unfairSorterPortLimits.put(NotOperator.class, 1);
         unfairSorterPortLimits.put(MultipleReadsFilterPattern.class, 25);
     }
-    public static final int specificPortLimit = 20;
-    public static final int approximateSorterStage1Depth = 5;
+    public static final int specificPortLimit = 3;
+    public static final int approximateSorterStage1Depth = 3;
 }
