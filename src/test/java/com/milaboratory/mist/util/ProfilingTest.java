73b0f8e017f1011ee0b0233fce8e051fc5026cef
Alexander230 <fire_2005@mail.ru>
2017-11-27 21:26:35 +0300

    Added test for aligner speed (WIP).

--- src/test/java/com/milaboratory/mist/util/ProfilingTest.java
+++ src/test/java/com/milaboratory/mist/util/ProfilingTest.java
@@ -1,10 +1,13 @@
 package com.milaboratory.mist.util;
 
+import com.milaboratory.core.alignment.*;
 import com.milaboratory.core.sequence.*;
+import com.milaboratory.mist.pattern.PatternAligner;
 import com.milaboratory.test.TestUtil;
 import org.junit.*;
 
 import static com.milaboratory.core.sequence.NucleotideSequenceCaseSensitive.fromNucleotideSequence;
+import static com.milaboratory.mist.util.CommonTestUtils.*;
 import static com.milaboratory.mist.util.DebugUtils.*;
 import static org.junit.Assert.*;
 
@@ -51,5 +54,35 @@ public class ProfilingTest {
         assertTrue(timeCounter.get("CreateOld") * 1.5 > timeCounter.get("CreateNew"));
         assertTrue(timeCounter.get("ToStringOld") * 10 > timeCounter.get("OldToNew"));
         assertTrue(timeCounter.get("ToStringOld") * 10 > timeCounter.get("NewToOld"));
+    }
+
+    @Test
+    public void alignerProfilingTest() throws Exception {
+        final LinearGapAlignmentScoring<NucleotideSequence> scoringOld = new LinearGapAlignmentScoring<>(
+                NucleotideSequence.ALPHABET, 0, -9, -10);
+        final PatternAndTargetAlignmentScoring scoringNew = new PatternAndTargetAlignmentScoring(
+                0, -9, -10, false,
+                (byte)34, (byte)0, 0);
+        final PatternAligner patternAligner = getTestPatternAligner();
+        for (int i = 0; i < 100000; i++) {
+            final NucleotideSequence seq1 = TestUtil.randomSequence(NucleotideSequence.ALPHABET,
+                    20, 40);
+            final NucleotideSequence seq2 = TestUtil.randomSequence(NucleotideSequence.ALPHABET,
+                    20, 40);
+            final NucleotideSequenceCaseSensitive seq1cs = fromNucleotideSequence(seq1, true);
+            final NSequenceWithQuality seq2q = new NSequenceWithQuality(seq2.toString());
+            final NucleotideSequenceCaseSensitive seq2cs = fromNucleotideSequence(seq2, true);
+
+            countExecutionTime("AlignGlobalOld", () -> {
+                Aligner.alignGlobal(scoringOld, seq1, seq2);
+                return null;
+            });
+            countExecutionTime("AlignGlobalNew", () -> {
+                PatternAndTargetAligner.alignGlobal(scoringNew, seq1cs, seq2q);
+                return null;
+            });
+        }
+
+        System.out.println(timeCounter.get("AlignGlobalOld") + " " + timeCounter.get("AlignGlobalNew"));
     }
 }
